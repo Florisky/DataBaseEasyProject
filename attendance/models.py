514474b79edbf15602fe0d13561eaa2fa121b28b
florisky <floriskyee@gmail.com>
2023-05-28 12:00:08 +0800

    modified models.py

--- attendance/models.py
+++ attendance/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import date
 
 class Employee(models.Model):
     GENDER_CHOICES = [
@@ -8,11 +10,15 @@ class Employee(models.Model):
     employee_id = models.CharField(max_length=10, unique=True)
     employee_name = models.CharField(max_length=50)
     employee_gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    employee_age = models.PositiveIntegerField(default=18)
+    employee_age = models.PositiveIntegerField()
 
     def clean(self):
         if self.employee_age < 18:
             raise models.ValidationError("年龄不能小于18岁。")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.employee_name
@@ -21,22 +27,41 @@ class Attendance(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     check_in_time = models.DateTimeField()
     check_out_time = models.DateTimeField()
+    isabsenteeism = models.BooleanField(default=False)
 
 class BusinessTrip(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField(default=date.today)
+    days = models.DurationField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.days = self.end_date - self.start_date
+        super().save(*args, **kwargs)
 
 class Leave(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField(default=date.today)
+    days = models.DurationField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.days = self.end_date - self.start_date
+        super().save(*args, **kwargs)
+
 
 class Overtime(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField(default=date.today)
+    hours = models.FloatField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        duration = self.end_date - self.start_date
+        self.hours = duration.days * 24 + duration.seconds // 3600
+        super().save(*args, **kwargs)
+
 
     class Meta:
-        verbose_name_plural = "Attendance Management System"
+        verbose_name_plural = "OverTime"
 
